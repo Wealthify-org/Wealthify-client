40f887de30e47f903324918d8fee570c3f60fa1b
Xeropho <andrey_cherkasov89@mail.ru>
2025-10-14 19:49:32 +0300

    dd routes enum replace string literals where applicable

--- src/components/UI/startHeader/StartHeader.tsx
+++ src/components/UI/startHeader/StartHeader.tsx
@@ -2,14 +2,15 @@ import classes from "./StartHeader.module.css"
 import Logo from "../logo/Logo"
 import BorderedLink from "../borderedButton/BorderedButton"
 import Link from "next/link"
+import { ROUTES } from "@/lib/routes"
 
 const StartHeader = () => {
   return (
     <header className={`${classes.headerBackground}`}>
       <Logo />
       <div className={classes.headerButtonsContainer}>
-        <Link href="/sign-in" className={classes.unborderedButton}>Sign in</Link>
-        <BorderedLink href="/sign-up">Sign up</BorderedLink>
+        <Link href={ROUTES.SIGN_IN} className={classes.unborderedButton}>Sign in</Link>
+        <BorderedLink href={ROUTES.SIGN_UP}>Sign up</BorderedLink>
       </div>
     </header>
   )
